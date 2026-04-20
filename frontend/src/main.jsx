366abe6fc848dcaeab6253e4005a00b528a22b76
Ashutosh9906 <ashutoshgandule1@gmail.com>
2026-04-20 23:10:06 +0530

    chnage are made to work

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,8 @@
-import { HashRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";  // ✅ changed
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
