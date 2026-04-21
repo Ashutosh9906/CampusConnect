1e9ffa6355e2ec50c372cc8c07b02454d7175157
Ashutosh9906 <ashutoshgandule1@gmail.com>
2026-04-21 12:23:35 +0530

    solved the errors 2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import eventRoutes from "./routers/eventRoutes.js";
 
 //middlewares
 app.use(cors({
-  origin: "https://campusconnect-3-kyzk.onrender.com",
+  origin: "campus-connect-kfagag2p5-ashutoshgandule1-6105s-projects.vercel.app",
   credentials: true
 }));
 // app.use(cors({
