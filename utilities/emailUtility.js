0ca52506b72e5254342bc93de9f10ed630544d0a
Ashutosh9906 <ashutoshgandule1@gmail.com>
2026-05-12 19:44:27 +0530

    removed nodemailer and implemeted brevo

--- utilities/emailUtility.js
+++ utilities/emailUtility.js
@@ -1,36 +1,42 @@
-import nodemailer from "nodemailer";
+import SibApiV3Sdk from 'sib-api-v3-sdk';
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 async function sendEmail(recipientEmail, { subject, html }) {
+
   try {
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
-      auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.APP_PASS,
-      },
+    sendSmtpEmail.sender = {
+      email: process.env.USER_EMAIL,
+      name: "CampusConnect"
+    };
 
-      family: 4
-    });
+    sendSmtpEmail.to = [
+      {
+        email: recipientEmail
+      }
+    ];
 
-    await transporter.verify();
-    console.log("SMTP Ready");
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html;
 
-    const info = await transporter.sendMail({
-      from: process.env.USER_EMAIL,
-      to: recipientEmail,
-      subject,
-      html,
-    });
+    const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
 
-    console.log("✅ Email Sent:", info.response);
+    console.log("✅ Email Sent:", response);
+
     return true;
 
-  } catch (error) {
+  } catch(error) {
+
     console.log("❌ Email Error:", error);
+
     return false;
   }
 }
